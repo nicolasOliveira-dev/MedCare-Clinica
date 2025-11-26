2913467d60519d0a03b30416d251cc3146b9d9ea
Nícolas Arthur Oliveira <nicolasoliveira.ss030@academico.ifsul.edu.br>
2025-11-26 19:56:06 -0300

    Update database password in Conexao.php
    Changed database password from '2308' to '1234'.

--- MediCare-main/app/Core/Conexao.php
+++ MediCare-main/app/Core/Conexao.php
@@ -3,7 +3,7 @@ class Conexao {
     private static $servidor = "localhost";
     private static $banco    = "clinica";
     private static $usuario  = "root";
-    private static $senha    = "2308";
+    private static $senha    = "1234";
 
 public static function getConexao()
  {
